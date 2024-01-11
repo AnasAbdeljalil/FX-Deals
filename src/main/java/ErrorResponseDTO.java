c19f77597c3a5cd515a96c9d795629944c30da9d
AnasAbdeljalil <39126543+AnasAbdeljalil@users.noreply.github.com>
2024-01-11 16:43:57 +0300

    Create ErrorResponseDTO.java

---
+++ src/main/java/ErrorResponseDTO.java
@@ -0,0 +1,41 @@
+package com.xx.fx.deals;
+
+public class ErrorResponseDTO {
+	private String code;
+	private String id;
+	private String message;
+	private String url;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
